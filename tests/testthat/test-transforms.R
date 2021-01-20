567d65f8a8e9efb9cf42a1db4338c1b99726361e
earamendia <eeear@leeds.ac.uk>
2021-01-20 18:32:32 +0100

    Tests for specify_MR_R and specify_MR_V functions up and passing

--- tests/testthat/test-transforms.R
+++ tests/testthat/test-transforms.R
@@ -5,7 +5,6 @@ context("Transforming PSUT")
 
 # Testing Domestic Technology Assumption functions
 
-
 test_that("find_list_dta_observations works", {
 
   A_B_path <- file.path("../../inst/A_B_data_full_2018_format.csv")
@@ -50,7 +49,6 @@ test_that("find_list_dta_observations works", {
 
   expect_true(is_list_correct)
 })
-
 
 
 
@@ -118,7 +116,124 @@ test_that("transform_to_dta works", {
 
 # Testing Global Market Assumption functions
 
+# Specifying R matrix
+test_that("specify_MR_R works", {
 
+  A_B_path <- file.path("../../inst/A_B_data_full_2018_format.csv")
+
+  AB_data <- A_B_path %>%
+    IEATools::load_tidy_iea_df() %>%
+    specify_all_revisited()
+
+  AB_resources_MR <- AB_data %>%
+    add_psut_matnames_epsilon() %>%
+    specify_MR_R()
+
+
+  expect_equal(dim(AB_resources_MR)[1], 3)
+
+  expect_equal(AB_resources_MR %>%
+                 dplyr::filter(Product == "{A}_Coking coal") %>%
+                 dplyr::select(E.dot) %>%
+                 dplyr::pull(),
+               5000)
+
+  expect_equal(AB_resources_MR %>%
+                 dplyr::filter(Product == "{A}_Crude oil") %>%
+                 dplyr::select(E.dot) %>%
+                 dplyr::pull(),
+               8500)
+
+  expect_equal(AB_resources_MR %>%
+                dplyr::filter(Flow.aggregation.point != "Total primary energy supply") %>%
+                dim(),
+               c(0, 12))
+
+  expect_equal(AB_resources_MR %>%
+                 dplyr::filter(! stringr::str_detect(Flow, "Resources")) %>%
+                 dim(),
+               c(0, 12))
+
+  expect_equal(AB_resources_MR %>%
+                 dplyr::filter(stringr::str_detect(Flow, "\\{B\\}") | stringr::str_detect(Product, "\\{B\\}")) %>%
+                 dim(),
+               c(0, 12))
+})
+
+
+# Specifying V matrix
+test_that("specify_MR_V works", {
+
+  A_B_path <- file.path("../../inst/A_B_data_full_2018_format.csv")
+
+  AB_data <- A_B_path %>%
+    IEATools::load_tidy_iea_df() %>%
+    specify_all_revisited()
+
+  AB_supply_MR <- AB_data %>%
+    add_psut_matnames_epsilon() %>%
+    specify_MR_V()
+
+
+  expect_equal(dim(AB_supply_MR),
+               c(18, 12))
+
+
+  expect_equal(AB_supply_MR %>%
+                 dplyr::filter(
+                   Flow == "{A}_Oil refineries",
+                   Product == "{A}_Heat"
+                 ) %>%
+                 dplyr::select(E.dot) %>%
+                 dplyr::pull(),
+               50)
+
+  expect_equal(AB_supply_MR %>%
+                 dplyr::filter(
+                   Flow == "{A}_Blast furnaces",
+                   Product == "{A}_Blast furnace gas"
+                 ) %>%
+                 dplyr::select(E.dot) %>%
+                 dplyr::pull(),
+               850)
+
+  expect_equal(AB_supply_MR %>%
+                 dplyr::filter(
+                   Flow == "{B}_Oil refineries",
+                   Product == "{B}_Heat"
+                 ) %>%
+                 dplyr::select(E.dot) %>%
+                 dplyr::pull(),
+               200)
+
+  expect_equal(AB_supply_MR %>%
+                 dplyr::filter(stringr::str_detect(Flow, "Imports")) %>%
+                 dim(),
+               c(0, 12))
+})
+
+
+# Specifying U and Y matrices
+# test_that("specify_MR_Y_U_gma works", {
+#
+#
+#
+#
+#
+# })
+
+
+
+
+# Whole transform_to_gma function works
+# test_that("transform_to_gma works"{
+#
+# })
+
+
+
+# Helper functions
+# Specify all etc.
 
 
 
