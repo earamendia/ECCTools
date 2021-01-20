616ac3a47ddefb6e34c1c7b74f75309feb4ba6cf
earamendia <eeear@leeds.ac.uk>
2021-01-20 18:55:39 +0100

    Tests for calc_total_consumption_by_product function added

--- tests/testthat/test-transforms.R
+++ tests/testthat/test-transforms.R
@@ -232,8 +232,188 @@ test_that("specify_MR_V works", {
 
 
 
-# Helper functions
-# Specify all etc.
+#### Helper functions ####
+
+test_that("calc_total_consumption_by_product works", {
+
+  A_B_path <- file.path("../../inst/A_B_data_full_2018_format.csv")
+
+  AB_data <- A_B_path %>%
+    IEATools::load_tidy_iea_df() %>%
+    specify_all_revisited()
+
+  total_consumption_by_product <- AB_data %>%
+    add_psut_matnames_epsilon() %>%
+    calc_total_consumption_by_product()
+
+  expect_equal(dim(total_consumption_by_product), c(21, 8))
+
+  expect_equal(total_consumption_by_product %>%
+                 dplyr::filter(
+                   Country == "A",
+                   Product == "Blast furnace gas"
+                 ) %>%
+                 dplyr::select(Total_Consumption_From_Func) %>%
+                 dplyr::pull(),
+               850)
+
+  expect_equal(total_consumption_by_product %>%
+                 dplyr::filter(
+                   Country == "A",
+                   Product == "Crude oil [of Oil and gas extraction]"
+                 ) %>%
+                 dplyr::select(Total_Consumption_From_Func) %>%
+                 dplyr::pull(),
+               5000)
+
+  expect_equal(total_consumption_by_product %>%
+                 dplyr::filter(
+                   Country == "A",
+                   Product == "Crude oil"
+                 ) %>%
+                 dplyr::select(Total_Consumption_From_Func) %>%
+                 dplyr::pull(),
+               8500)
+
+  expect_equal(total_consumption_by_product %>%
+                 dplyr::filter(
+                   Country == "B",
+                   Product == "Crude oil [of Oil and gas extraction]"
+                 ) %>%
+                 dplyr::select(Total_Consumption_From_Func) %>%
+                 dplyr::pull(),
+               3000)
+})
+
+
+# Total consumption by product, year, country
+# total_consumption_by_product <- AB_data %>%
+#   add_psut_matnames_epsilon() %>%
+#   filter((matnames == "Y" | matnames == "U_feed" | matnames == "U_EIOU"), ! str_detect(Flow, "Exports")) %>% # There shouldn't be anymore exports where there are imports, now.
+#   mutate(
+#     E.dot = case_when(
+#       matnames == "U_feed" ~ - E.dot,
+#       matnames == "U_EIOU" ~ - E.dot,
+#       TRUE ~ E.dot
+#     )
+#   ) %>%
+#   group_by(Country, Method, Energy.type, Last.stage, Year, Product, Unit) %>%
+#   summarise(
+#     total_consumption = sum(E.dot)
+#   ) %>%
+#   View()
+
+# Imports by product, year, country
+# imports <- AB_data %>%
+#   filter(str_detect(Flow, "Imports")) %>%
+#   rename(Imports = E.dot) %>%
+#   print()
+
+# Share of imported products by product, year, country
+# share_imports_by_product <- total_consumption_by_product %>%
+#   left_join(imports, by = c("Country", "Method", "Energy.type", "Last.stage", "Year", "Product")) %>%
+#   select(-Ledger.side, -matnames, -Flow.aggregation.point, -Flow) %>%
+#   mutate(
+#     Share_Imports = case_when(
+#       is.na(Imports) ~ 0,
+#       !is.na(Imports) ~ Imports / total_consumption
+#     )
+#   ) %>%
+#   print()
+
+
+# Specifying imported products in Y, U_EIOU, U_feed
+
+# defining_imported_products <- AB_tidy_data %>%
+#   filter((matnames == "Y" | matnames == "U_feed" | matnames == "U_EIOU"), ! str_detect(Flow, "Exports")) %>%
+#   left_join(
+#     share_imports_by_product, by = c("Country", "Year", "Product", "Method", "Energy.type", "Last.stage")
+#   ) %>%
+#   mutate(
+#     Domestic = E.dot * (1 - Share_Imports),
+#     Imported = E.dot * Share_Imports
+#   ) %>%
+#   select(-E.dot, -total_consumption, -Imports, -Share_Imports) %>%
+#   pivot_longer(cols = c("Domestic", "Imported"), names_to = "Origin", values_to = "E.dot") %>%
+#   relocate(Origin, .after = Product) %>%
+#   filter(E.dot != 0) %>%
+#   print()
+
+
+# Creating trade matrices data frame
+# total_exports <- AB_tidy_data %>%
+#   filter(str_detect(Flow, "Exports")) %>%
+#   mutate(
+#     E.dot = abs(E.dot)
+#   ) %>%
+#   group_by(Method, Energy.type, Last.stage, Year, Ledger.side, Flow.aggregation.point, Product) %>%
+#   summarise(
+#     Total_Exports = sum(E.dot)
+#   ) %>%
+#   print()
+
+
+# This is not yet a matrix....
+# share_exports <- AB_tidy_data %>%
+#   filter(str_detect(Flow, "Exports")) %>%
+#   mutate(
+#     E.dot = abs(E.dot)
+#   ) %>%
+#   left_join(total_exports, by = c("Method", "Energy.type", "Last.stage", "Year", "Ledger.side", "Flow.aggregation.point", "Product")) %>%
+#   mutate(
+#     Share_Exports = E.dot / Total_Exports
+#   ) %>%
+#   rename(Provenience = Country) %>%
+#   print()
+
+
+# So here we create a data frame equivalent to a matrix with n*n countries. Also A to A etc. Missing values are implicitly 0 shares.
+# share_exports_by_origin_destination <- share_exports %>%
+#   expand_grid(Country = AB_tidy_data %>%
+#                 expand(Country) %>%
+#                 pull()) %>%
+#   mutate(
+#     Share_Exports = case_when(
+#       Provenience == Country ~ 0,
+#       TRUE ~ Share_Exports
+#     )
+#   ) %>%
+#   relocate(Country, .after = Provenience) %>%
+#   select(- Ledger.side, -Flow.aggregation.point, -Flow, -Unit, -E.dot, -matnames, -Total_Exports) %>%
+#   print()
+#
+#
+# testing <- share_exports_by_origin_destination %>%
+#   add_row(Provenience = "C",
+#           Country = "B",
+#           Method = "PCM",
+#           Energy.type = "E",
+#           Last.stage = "Final",
+#           Year = 2018,
+#           Product = "Natural gas [of Oil and gas extraction]",
+#           Share_Exports = 0.5) %>%
+#   print()
+
+# Testing the trade matrix
+
+# share_exports_by_origin_destination %>%
+#   group_by(Country, Method, Energy.type, Last.stage, Year, Product) %>%
+#   summarise(sum_shares = sum(Share_Exports)) %>%
+#   print()
+#
+#
+#
+# test_that("calc_imports_by_product works"{
+#
+#
+# })
+#
+#
+# test_that("calc_share_imports_by_products works"{
+#
+#
+# })
+
 
 
 
