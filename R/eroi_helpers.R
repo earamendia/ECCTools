c01a1352282b0674d6b5d68e5337eacdebee6233
earamendia <eeear@leeds.ac.uk>
2021-02-10 19:18:14 +0100

    calc_all_products_use_by_group() function up and running

--- R/eroi_helpers.R
+++ R/eroi_helpers.R
@@ -154,11 +154,110 @@ calc_primary_products_use_by_group <- function(.tidy_iea_df,
 }
 
 
+# Calculates use of products by main group (coal vs oil&gas)
+calc_all_products_use_by_group <- function(.tidy_iea_df,
+                                           include_non_energy_uses = FALSE,
+                                           list_oil_products = IEATools::oil_and_oil_products,
+                                           list_coal_products = IEATools::coal_and_coal_products,
+                                           list_gas_products = list(natural_gas = "Natural gas"),
+                                           list_non_energy_flows = IEATools::non_energy_flows,
+                                           exports = IEATools::interface_industries$exports,
+                                           losses = IEATools::tfc_compare_flows$losses,
+                                           country = IEATools::iea_cols$country,
+                                           method = IEATools::iea_cols$method,
+                                           energy_type = IEATools::iea_cols$energy_type,
+                                           last_stage = IEATools::iea_cols$last_stage,
+                                           year = IEATools::iea_cols$year,
+                                           product = IEATools::iea_cols$product,
+                                           unit = IEATools::iea_cols$unit,
+                                           flow = IEATools::iea_cols$flow,
+                                           e_dot = IEATools::iea_cols$e_dot,
+                                           matnames = IEATools::mat_meta_cols$matnames,
+                                           Y_mat = IEATools::psut_cols$Y,
+                                           U_eiou_mat = IEATools::psut_cols$U_eiou,
+                                           product.group = "Product.Group",
+                                           total_product_use = "Total_Product_Use"){
+
+  if (isFALSE(include_non_energy_uses)){
+
+    to_return <- .tidy_iea_df %>%
+      dplyr::filter(.data[[product]] %in% c(list_coal_products, list_oil_products, list_gas_products)) %>%
+      dplyr::filter(
+        ! (stringr::str_detect(.data[[flow]], exports) | stringr::str_detect(.data[[flow]], losses))
+      ) %>%
+      dplyr::filter(matnames == Y_mat | matnames == U_eiou_mat) %>%
+      dplyr::filter(! .data[[flow]] %in% list_non_energy_flows) %>%
+      dplyr::mutate(
+        "{product.group}" := dplyr::case_when(
+          .data[[product]] %in% c(list_oil_products, list_gas_products) ~ "Oil and gas products",
+          .data[[product]] %in% list_coal_products ~ "Coal products"
+        ),
+        "{e_dot}" := dplyr::case_when(
+          .data[[matnames]] == U_eiou_mat ~ -.data[[e_dot]],
+          TRUE ~ .data[[e_dot]]
+        )
+      ) %>%
+      dplyr::group_by(.data[[country]], .data[[method]], .data[[energy_type]], .data[[last_stage]], .data[[year]], .data[[product.group]], .data[[unit]]) %>%
+      dplyr::summarise(
+        "{total_product_use}" := sum(.data[[e_dot]])
+      )
+
+    return(to_return)
+
+  } else if (isTRUE(include_non_energy_uses)){
+
+    to_return <- .tidy_iea_df %>%
+      dplyr::filter(.data[[product]] %in% c(list_coal_products, list_oil_products, list_gas_products)) %>%
+      dplyr::filter(
+        ! (stringr::str_detect(.data[[flow]], exports) | stringr::str_detect(.data[[flow]], losses))
+      ) %>%
+      dplyr::filter(matnames == Y_mat | matnames == U_eiou_mat) %>%
+      dplyr::mutate(
+        "{product.group}" := dplyr::case_when(
+          .data[[product]] %in% c(list_oil_products, list_gas_products) ~ "Oil and gas products",
+          .data[[product]] %in% list_coal_products ~ "Coal products"
+        ),
+        "{e_dot}" := dplyr::case_when(
+          .data[[matnames]] == U_eiou_mat ~ -.data[[e_dot]],
+          TRUE ~ .data[[e_dot]]
+        )
+      ) %>%
+      dplyr::group_by(.data[[country]], .data[[method]], .data[[energy_type]], .data[[last_stage]], .data[[year]], .data[[product.group]], .data[[unit]]) %>%
+      dplyr::summarise(
+        "{total_product_use}" := sum(.data[[e_dot]])
+      )
+
+    return(to_return)
+
+  } else {
+    stop("The include_non_energy_uses argument must be either TRUE or FALSE.")
+  }
+}
 
 
-
-calc_all_products_use_by_group <- function(){
-
+# Calculates total primary fossil fuel use
+calc_primary_ff_use() <- function(.tidy_iea_df,
+                                  include_non_energy_uses = FALSE,
+                                  list_primary_oil_products = IEATools::primary_oil_products,
+                                  list_primary_coal_products = IEATools::primary_coal_products,
+                                  list_primary_gas_products = list(natural_gas = "Natural gas"),
+                                  list_non_energy_flows = IEATools::non_energy_flows,
+                                  exports = IEATools::interface_industries$exports,
+                                  losses = IEATools::tfc_compare_flows$losses,
+                                  country = IEATools::iea_cols$country,
+                                  method = IEATools::iea_cols$method,
+                                  energy_type = IEATools::iea_cols$energy_type,
+                                  last_stage = IEATools::iea_cols$last_stage,
+                                  year = IEATools::iea_cols$year,
+                                  product = IEATools::iea_cols$product,
+                                  unit = IEATools::iea_cols$unit,
+                                  flow = IEATools::iea_cols$flow,
+                                  e_dot = IEATools::iea_cols$e_dot,
+                                  matnames = IEATools::mat_meta_cols$matnames,
+                                  Y_mat = IEATools::psut_cols$Y,
+                                  U_eiou_mat = IEATools::psut_cols$U_eiou,
+                                  product.group = "Product.Group",
+                                  total_product_use = "Total_Product_Use"){
 
   if (isFALSE(include_non_energy_uses)){
 
@@ -171,21 +270,29 @@ calc_all_products_use_by_group <- function(){
 
 }
 
-calc_primary_ff_use() <- function(){
-
-  if (isFALSE(include_non_energy_uses)){
-
-  } else if (isTRUE(include_non_energy_uses)){
-
-  } else {
-    stop("The include_non_energy_uses argument must be either TRUE or FALSE.")
-  }
-
-
-}
-
-
-calc_ff_use() <- function(){
+# Calculates total fossil fuel use
+calc_ff_use() <- function(.tidy_iea_df,
+                          include_non_energy_uses = FALSE,
+                          list_oil_products = IEATools::oil_and_oil_products,
+                          list_coal_products = IEATools::coal_and_coal_products,
+                          list_gas_products = list(natural_gas = "Natural gas"),
+                          list_non_energy_flows = IEATools::non_energy_flows,
+                          exports = IEATools::interface_industries$exports,
+                          losses = IEATools::tfc_compare_flows$losses,
+                          country = IEATools::iea_cols$country,
+                          method = IEATools::iea_cols$method,
+                          energy_type = IEATools::iea_cols$energy_type,
+                          last_stage = IEATools::iea_cols$last_stage,
+                          year = IEATools::iea_cols$year,
+                          product = IEATools::iea_cols$product,
+                          unit = IEATools::iea_cols$unit,
+                          flow = IEATools::iea_cols$flow,
+                          e_dot = IEATools::iea_cols$e_dot,
+                          matnames = IEATools::mat_meta_cols$matnames,
+                          Y_mat = IEATools::psut_cols$Y,
+                          U_eiou_mat = IEATools::psut_cols$U_eiou,
+                          product.group = "Product.Group",
+                          total_product_use = "Total_Product_Use"){
 
   if (isFALSE(include_non_energy_uses)){
 
