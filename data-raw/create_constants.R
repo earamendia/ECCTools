022b2d7ef93529af2625f2662235f561b6a49a0f
earamendia <eeear@leeds.ac.uk>
2021-03-31 13:47:35 +0200

    Added create_constants.R script

---
+++ data-raw/create_constants.R
@@ -0,0 +1,16 @@
+# This script creates constants and saves them in the right locations.
+# If there are any changes to these constants,
+# source this script before building the package.
+
+
+#
+# Reading and saving default AB data
+#
+
+A_B_path <- system.file("A_B_data_full_2018_format.csv", package = "ECCTools")
+
+tidy_AB_data <- A_B_path %>%
+  IEATools::load_tidy_iea_df() %>%
+  IEATools::specify_all()
+
+usethis::use_data(tidy_AB_data, overwrite = TRUE)
