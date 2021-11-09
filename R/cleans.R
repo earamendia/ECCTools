e1e17435db4f59fd4241c26b13511cf261e0bafc
earamendia <eeear@leeds.ac.uk>
2021-11-09 20:55:43 +0100

    Input and output flows sorted out for the specify_elect_heat_renewables function. Now EIOU flows remain to be done.

--- R/cleans.R
+++ R/cleans.R
@@ -466,6 +466,16 @@ specify_elect_heat_renewables <- function(.tidy_iea_df,
                                           product = IEATools::iea_cols$product,
                                           e_dot = IEATools::iea_cols$e_dot,
                                           unit = IEATools::iea_cols$unit,
+                                          # Flow aggregation points
+                                          transformation_processes = IEATools::aggregation_flows$transformation_processes,
+                                          eiou_flows = IEATools::aggregation_flows$energy_industry_own_use,
+                                          # Elec and heat producing industries
+                                          main_act_prod_elect = IEATools::main_act_plants$main_act_prod_elect_plants,
+                                          main_act_prod_chp = IEATools::main_act_plants$main_act_prod_chp_plants,
+                                          main_act_prod_heat = IEATools::main_act_plants$main_act_prod_heat_plants,
+                                          autoprod_elect = "Autoproducer electricity plants",
+                                          autoprod_chp = "Autoproducer CHP plants",
+                                          autoprod_heat = "Autoproducer heat plants",
                                           # Energy product names
                                           hydro = "Hydro",
                                           geothermal = "Geothermal",
@@ -473,7 +483,10 @@ specify_elect_heat_renewables <- function(.tidy_iea_df,
                                           solar_th = "Solar thermal",
                                           tide_wave_ocean = "Tide, wave and ocean",
                                           wind = "Wind",
+                                          electricity = "Electricity",
+                                          heat = "Heat",
                                           # Constants - see WEEB documentation
+                                          ratio_hydro_to_input = 1,
                                           ratio_solar_PV_to_input = 1,
                                           ratio_solar_thermal_elect_to_input = 0.33,
                                           ratio_solar_thermal_heat_to_input = 1,
@@ -492,6 +505,90 @@ specify_elect_heat_renewables <- function(.tidy_iea_df,
                                     "{electricity}" := NA,
                                     "{heat}" := NA)
 
+  # Defining product list:
+  renewable_product_list <- c(hydro, geothermal, solar_pv, solar_th, tide_wave_ocean, wind)
+  products_list <- c(hydro, geothermal, solar_pv, solar_th, tide_wave_ocean, wind, electricity, heat)
+
+  # Defining list of elect and heat producer activities:
+  elect_heat_producer_industries <- c(main_act_prod_elect, main_act_prod_chp, main_act_prod_heat, autoprod_elect, autoprod_chp, autoprod_heat)
+
+  # Here we keep only the flows that we are going to modify:
+  intermediary_modified_flows <- .tidy_iea_df %>%
+    dplyr::filter(
+      .data[[flow_aggregation_point]] == transformation_processes &
+        ((.data[[flow]] %in% elect_heat_producer_industries & .data[[product]] %in% products_list))
+    ) %>%
+    tidyr::pivot_wider(names_from = .data[[product]], values_from = .data[[e_dot]]) %>%
+    dplyr::select(-tidyselect::any_of({e_dot}))
+
+  # Storing the names
+  names_intermediary_modified_flows <- names(intermediary_modified_flows)
+
+  # Modifying output and input flows:
+  modified_flows <- intermediary_modified_flows %>%
+    # Making sure all products in products_tibble exist as columns prior to manipulating the tibble:
+    tibble::add_column(!!products_tibble[! names(products_tibble) %in% names_intermediary_modified_flows]) %>%
+    # Putting zeros where we have NAs
+    dplyr::mutate(
+      "{hydro}" := tidyr::replace_na(.data[[hydro]], 0),
+      "{geothermal}" := tidyr::replace_na(.data[[geothermal]], 0),
+      "{solar_pv}" := tidyr::replace_na(.data[[solar_pv]], 0),
+      "{solar_th}" := tidyr::replace_na(.data[[solar_th]], 0),
+      "{tide_wave_ocean}" := tidyr::replace_na(.data[[tide_wave_ocean]], 0),
+      "{wind}" := tidyr::replace_na(.data[[wind]], 0),
+      "{electricity}" := tidyr::replace_na(.data[[electricity]], 0),
+      "{heat}" := tidyr::replace_na(.data[[heat]], 0)
+    ) %>%
+    # Now come the interesting bit, modifying flows output flows
+    dplyr::mutate(
+      # Determining the share of electricity against heat, when heat is involved:
+      "{share_elect_output_From_Func}" := .data[[electricity]] / (.data[[electricity]] + .data[[heat]]),
+      # Modifying input flows:
+      "{electricity}" := dplyr::case_when(
+        (.data[[flow]] == main_act_prod_elect | .data[[flow]] == autoprod_elect) ~ .data[[electricity]] + .data[[hydro]] * ratio_hydro_to_input +
+          .data[[geothermal]] * ratio_geothermal_elect_to_input + .data[[solar_pv]] * ratio_solar_PV_to_input +
+          .data[[solar_th]] * ratio_solar_thermal_elect_to_input + .data[[tide_wave_ocean]] * ratio_tidal_wave_to_input +
+          .data[[wind]] * ratio_wind_to_input,
+        (.data[[flow]] == main_act_prod_chp | .data[[flow]] == autoprod_chp) ~ .data[[electricity]] +
+          .data[[geothermal]] * ratio_geothermal_elect_to_input * .data[[share_elect_output_From_Func]],
+        (.data[[flow]] == main_act_prod_heat | .data[[flow]] == autoprod_heat) ~ .data[[electricity]]
+      ),
+      "{heat}" := dplyr::case_when(
+        (.data[[flow]] == main_act_prod_elect | .data[[flow]] == autoprod_elect) ~ .data[[heat]],
+        (.data[[flow]] == main_act_prod_chp | .data[[flow]] == autoprod_chp) ~ .data[[heat]] +
+          .data[[geothermal]] * ratio_geothermal_heat_to_input * (1 - .data[[share_elect_output_From_Func]]),
+        (.data[[flow]] == main_act_prod_heat | .data[[flow]] == autoprod_heat) ~ .data[[heat]] + .data[[geothermal]] * ratio_geothermal_heat_to_input +
+          .data[[solar_th]] * ratio_solar_thermal_heat_to_input
+      ),
+      # Modifying output flows:
+      Electricity_Renewables = dplyr::case_when(
+        (.data[[flow]] == main_act_prod_elect | .data[[flow]] == autoprod_elect) ~ - (.data[[hydro]] * ratio_hydro_to_input +
+          .data[[geothermal]] * ratio_geothermal_elect_to_input + .data[[solar_pv]] * ratio_solar_PV_to_input + .data[[solar_th]] * ratio_solar_thermal_elect_to_input +
+            .data[[tide_wave_ocean]] * ratio_tidal_wave_to_input + .data[[wind]] * ratio_wind_to_input),
+        (.data[[flow]] == main_act_prod_chp | .data[[flow]] == autoprod_chp) ~ - .data[[geothermal]] * ratio_geothermal_elect_to_input * .data[[share_elect_output_From_Func]],
+
+        (.data[[flow]] == main_act_prod_heat | .data[[flow]] == autoprod_heat) ~ 0
+      ),
+      Heat_Renewables = dplyr::case_when(
+        (.data[[flow]] == main_act_prod_elect | .data[[flow]] == autoprod_elect) ~ 0,
+        (.data[[flow]] == main_act_prod_chp | .data[[flow]] == autoprod_chp) ~ - .data[[geothermal]] * ratio_geothermal_heat_to_input * (1 - .data[[share_elect_output_From_Func]]),
+        (.data[[flow]] == main_act_prod_heat | .data[[flow]] == autoprod_heat) ~ - (.data[[geothermal]] * ratio_geothermal_heat_to_input + .data[[solar_th]] * ratio_solar_thermal_heat_to_input)
+      ),
+    ) %>%
+    dplyr::select(-.data[[share_elect_output_From_Func]]) %>%
+    tidyr::pivot_longer(cols = c({electricity}, {heat}, {hydro}, {geothermal}, {solar_pv}, {solar_th}, {tide_wave_ocean}, {wind}, "Electricity_Renewables", "Heat_Renewables")
+                        , values_to = {e_dot}, names_to = {product}) %>%
+    dplyr::filter(.data[[e_dot]] != 0) %>%
+    dplyr::mutate(
+      "{flow}" := dplyr::case_when(
+        stringr::str_detect(.data[[product]], "Renewables") ~ "Renewable energy plant",
+        .data[[product]] %in% renewable_product_list ~ "Renewable energy plant",
+        TRUE ~ .data[[flow]]
+      )
+    )
+
+
+  # Modifying EIOU flows
 
 
 
@@ -502,6 +599,8 @@ specify_elect_heat_renewables <- function(.tidy_iea_df,
   # Returning modified data frame
   return(to_return)
 }
+
+
 
 
 # Second, specifying elec/heat flows from oil products, natural gas, and coal products
