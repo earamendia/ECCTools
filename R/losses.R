ddb9c5332be28cfc977316ba1d0f688e41398471
earamendia <eeear@leeds.ac.uk>
2022-06-13 11:28:55 +0100

    First full go at specify_losses_as_industry() function, including documentation

--- R/losses.R
+++ R/losses.R
@@ -1,4 +1,30 @@
 
+#' Specifies losses as a transformation industry
+#'
+#' Losses are originally treated as a final demand flow in the IEA WEEB balances. This function allows analysts to redefine losses
+#' as a transformation industry, which takes as input "Product (before Losses)" and provides "Product".
+#' Hence, final demand sectors still consume "Product", but where relevant, products go first through a "Losses (of Product)" industry.
+#'
+#' @param .tidy_iea_df The `.tidy__iea_df` for which losses need to be specified as transformation industry.
+#' @param country,method,energy_type,last_stage,year,flow,product,unit,e_dot See `IEATools::iea_cols`.
+#' @param matnames The name of the matrix name column in the input data frame.
+#' @param losses The name of the "Losses" flows in the input data frame.
+#'               Default is `IEATools::tfc_compare_flows$losses`.
+#' @param imports The name of "Imports" flows in the input data frame.
+#'                Default is `IEATools::interface_industries$imports`.
+#' @param stat_diffs The name of "Statistical differences" flows in the input data frame.
+#'                   Default is `IEATools::tfc_compare_flows$statistical_differences`.
+#' @param transformation_processes The name of transformation processes in the flow aggregation point column.
+#'                                 Default is `IEATools::tfc_compare_flows$transformation_processes`.
+#' @param supply The name of the supply ledger side.
+#'               Default is `IEATools::ledger_sides$supply`.
+#' @param observation_string The name of a temporary column identifying the observation.
+#'                           Default is "Observation_String".
+#'
+#' @return A `.tidy_iea_df` with losses specified as a transformation industry.
+#' @export
+#'
+#' @examples
 specify_losses_as_industry <- function(.tidy_iea_df,
                                        country = IEATools::iea_cols$country,
                                        method = IEATools::iea_cols$method,
@@ -76,7 +102,7 @@ specify_losses_as_industry <- function(.tidy_iea_df,
     dplyr::filter(! stringr::str_detect(.data[[flow]], stat_diffs)) %>%
     dplyr::group_by(.data[[country]], .data[[method]], .data[[energy_type]], .data[[last_stage]], .data[[year]], .data[[product]], .data[[unit]]) %>%
     dplyr::summarise(
-      "{e_dot}" := sum(.data[[e_dot]]) # losses have negative sign so we can do like this.
+      "{e_dot}" := sum(.data[[e_dot]])
     ) %>%
     dplyr::mutate(
       "{ledger_side}" := supply,
@@ -86,9 +112,32 @@ specify_losses_as_industry <- function(.tidy_iea_df,
       "{matnames}" := "U_feed"
     )
 
+  # Need to create data frames with imports and statistical differences located in V matrix
+  imports_stat_diffs_V_df <- .tidy_iea_df %>%
+    dplyr::filter(
+      stringr::str_c(.data[[country]], "_", .data[[method]], "_", .data[[energy_type]], "_", .data[[last_stage]], "_",
+                     .data[[year]], "_", .data[[product]], "_") %in% observations_losses
+    ) %>%
+    dplyr::filter(matnames == "V" & (stringr::str_detect(.data[[flow]], imports) | stringr::str_detect(.data[[flow]], stat_diffs)))
+
+  # Check that all flows supplied by losses remain positive. Else there is an issue.
+  supply_industry_losses_df %>%
+    dplyr::filter(.data[[e_dot]] < 0) %>%
+    nrow() %>%
+    testthat::expect_equal(0)
 
   # CAREFUL HERE ON HOW TO DEFINE THIS BIT TO AVOID DOUBLE ACCOUNTING FLOWS!!!!
-  tidy_iea_df_with_losses_specified <- .tidy_iea_df
+  tidy_iea_df_with_losses_specified <- .tidy_iea_df %>%
+    dplyr::filter(
+      ! ((stringr::str_c(.data[[country]], "_", .data[[method]], "_", .data[[energy_type]], "_", .data[[last_stage]], "_",
+                       .data[[year]], "_", .data[[product]], "_") %in% observations_losses) & (.data[[matnames == "V"]]))
+    ) %>%
+    dplyr::bind_rows(
+      modified_supply_excluding_losses,
+      supply_industry_losses_df,
+      inputs_to_losses_industry_df,
+      imports_stat_diffs_V_df
+    )
 
   return(tidy_iea_df_with_losses_specified)
 }
